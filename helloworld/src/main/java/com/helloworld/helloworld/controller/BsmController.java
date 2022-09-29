e302bd0c30856a1f280c8c0e663f03b3fae6e548
Nayara de Carvalho Silva <112405575+ncarvalhosilva@users.noreply.github.com>
2022-09-29 15:22:56 -0300

    alteração de escrita

--- helloworld/src/main/java/com/helloworld/helloworld/controller/BsmController.java
+++ helloworld/src/main/java/com/helloworld/helloworld/controller/BsmController.java
@@ -10,7 +10,7 @@ public class BsmController {
 	
 	@GetMapping
 	public String bsmGeneration() {
-		return "Conheça as 8 BSM's da Generation <br><br>COMPETÊNCIAS COMPORTAMENTAIS:"
+		return "A seguir veja as BSM's trabalhadas na Generation Brasil<br><br>COMPETÊNCIAS COMPORTAMENTAIS:"
 				+ "<br><li>Comunicação<br><li>Proatividade<br><li>Orientação ao detalhe"
 				+ "<br><li>Trabalho em equipe<br><br>MENTALIDADES:<br>"
 				+ "<li>Orientação ao futuro<br><li>Responsabilidade pessoal"
--- helloworld/src/main/java/com/helloworld/helloworld/controller/HelloWorldController.java
+++ helloworld/src/main/java/com/helloworld/helloworld/controller/HelloWorldController.java
@@ -12,4 +12,5 @@ public class HelloWorldController {
 	public String helloWord() {
 		return "Hello World!";
 	}
+	
 }
